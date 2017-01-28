a1cab631fe15f1af80a720eaebcf6c7e46bb41cb
MarcosCosmos <m.cosmos42@gmail.com>
2017-01-28 18:50:22 +1100

    got static file serving going

--- src/controllers/server/main.js
+++ src/controllers/server/main.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import fs from 'fs';
 var app = express();
-app.use(express.static('/dist/public'));
+app.use(express.static('dist/public/'));
 app.use('/angular/', express.static('dist/controllers/client'));
 app.get('/', function (req, res) {
 	fs.readFile('dist/views/static_themeing/index.html', 'utf-8',
