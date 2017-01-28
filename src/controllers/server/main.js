bdfcdc91015bb5d6e300563abcd629f06fe15dd6
MarcosCosmos <m.cosmos42@gmail.com>
2017-01-28 18:47:31 +1100

    now serving the plain html, but not the resource files

--- src/controllers/server/main.js
+++ src/controllers/server/main.js
@@ -1,21 +1,19 @@
 import express from 'express';
 import fs from 'fs';
 var app = express();
-app.use('/', express.static('/public'));
-app.use('/angular/', express.static('/controllers/angular'));
+app.use(express.static('/dist/public'));
+app.use('/angular/', express.static('dist/controllers/client'));
 app.get('/', function (req, res) {
-	fs.readFile('/views/static_themeing/index.html',
+	fs.readFile('dist/views/static_themeing/index.html', 'utf-8',
 		function(err, data) {
 			if (err) {
-				throw "hi";
+				res.send(process.cwd() + " - couldn't find the file");
 			} else {
 				res.send(data);
 			}
 		}
 	);
-}
-);
+});
 app.listen(8081, function() {
 	console.log('Listening on  8081');
-}
-);
+});
