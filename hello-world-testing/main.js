7085b86c16e702e224b6b5db14bf10d41179eaee
MarcosCosmos <m.cosmos42@gmail.com>
2017-01-28 12:21:33 +1100

    Hello world now uses expressjs

--- hello-world-testing/main.js
+++ hello-world-testing/main.js
@@ -1,13 +1,11 @@
-var http = require('http');
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World 42!\n');
-}).listen(8081);
+var express = require('express');
+var app = express();
 
-// Console will print the message
-console.log('Server running at http://localhost:8081/');
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+}
+);
+app.listen(8081, function() {
+	console.log('Listening on  8081');
+}
+);
